746266beae3ff48bf47e883c074537b21f502085
AlexArts <a_arz@mail.ru>
2023-06-26 18:29:47 +0400

    Поправил шаблон

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,10 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
