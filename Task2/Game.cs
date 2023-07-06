2573d24dca88e994a486e3c8dd7f7885042ae098
Alex Arts <a_arz@mail.ru>
2023-07-06 17:05:57 +0400

    Add constructor and method CreateGameState

--- Task2/Game.cs
+++ Task2/Game.cs
@@ -1,8 +1,30 @@
 ﻿using System;
 
-public class Class1
+public class Game
 {
-	public Class1()
+    #region Поля и свойства
+    private int resolution;
+    private string[,] state = new string[resolution, resolution];
+    #endregion
+    #region Конструктор
+    public Game(int resolution)
 	{
-	}
+        this.resolution = resolution;
+        this.state = CreateGameState();
+    }
+    #endregion
+    #region Методы
+    private string[,] CreateGameState()
+    {
+        string[,] field = new string[resolution, resolution];
+        for (int i = 0; i < resolution; i += 1)
+        {
+            for (int j = 0; j < resolution; j += 1)
+            {
+                field[i, j] = " ";
+            }
+        }
+        return field;
+    }
+    #endregion
 }
