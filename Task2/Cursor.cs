6fe920c440299ad5d277c788cd51f1dcfd4458f7
Alex Arts <a_arz@mail.ru>
2023-07-07 17:11:33 +0400

    Rename class Render to IO, add classes Cursor, Combination

---
+++ Task2/Cursor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Cursor
+{
+	private int x;
+	private int y;
+	private int resolution;
+
+	public Cursor(int resolution)
+	{
+		this.x = 0;
+		this.y = 0;
+		this.resolution = resolution;
+	}
+
+	public int GetX ()
+	{
+		return x;
+	}
+
+    public int GetY()
+    {
+        return y;
+    }
+
+	public void SetX (int x)
+	{
+		this.x = x;
+	}
+
+    public void SetY(int y)
+    {
+        this.x = y;
+    }
+
+    public void MoveXRight()
+	{
+		this.SetX(this.GetX() < resoluton - 1 ? (this.GetX() + 1) : 0);
+    }
+
+    public void MoveXLeft()
+	{
+		this.SetX(this.GetX() >= 1 ? (this.GetX() - 1) : resoluton - 1);
+	}
+
+    public void MoveYDown() => y < resoluton - 1 ? (y + 1) : 0;
+
+    public void MoveYUp() => y >= 1 ? (y - 1) : resoluton - 1;
+}
