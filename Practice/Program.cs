c723c2af594cc1d0cc7b86e31c0a32f5a4969a85
Alex Arts <a_arz@mail.ru>
2023-08-02 17:50:46 +0400

    Did part of the practice 1

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Практика. Основы С# 1
+            int age;
+            string name;
+            string companyName;
+            bool isBoolVariable;
+            double weightOfHuman;
+            #endregion
+
+            #region Практика. Основы С# 2
+            int ageOfHuman = 33;
+            string nameOfAlex = "Alex";
+            double temperature = 30.1;
+            bool isWoman = false;
+            #endregion
+
+            #region Практика. Основы С# 3
+            for(var i = 0; i < 10; i += 1)
+            {
+                Console.WriteLine(i);
+            }
+
+            var j = 0;
+            while (j < 10)
+            {
+                Console.WriteLine(j);
+                j += 1;
+            }
+
+            var k = 0;
+            do
+            {
+                Console.WriteLine(k);
+                k += 1;
+            } while (k < 10);
+
+
+
+            #endregion
+
+            #region Практика. Основы С# 4
+
+            #endregion
+
+            #region Практика. Основы С# 5
+
+            #endregion
+
+        }
+    }
+}
