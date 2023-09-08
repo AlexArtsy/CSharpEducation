81322a12111d7b41eb1037b43d97ff40041f1f19
Alex Arts <a_arz@mail.ru>
2023-09-08 17:57:03 +0400

    Add user id

--- Task3/Phonebook.cs
+++ Task3/Phonebook.cs
@@ -8,5 +8,44 @@ namespace Task3
 {
     internal class Phonebook
     {
+        #region Поля
+        private static Phonebook instance;
+        #endregion
+        #region Свойства
+        #endregion
+        #region Методы
+        public bool AddNewPhoneNumber(string phoneNumber)
+        {
+            return true;
+        }
+
+        public bool DeletePhoneNumber(string phoneNumber)
+        {
+            return true;
+        }
+
+        public Subscriber SearchSubscriberByPhoneNumber(string phoneNumber)
+        {
+
+        }
+
+        public List<string> GetPhoneNumbersBySubscriber(Subscriber user)
+        {
+
+        }
+
+        public static Phonebook GetInstance()
+        {
+            if (instance == null)
+                instance = new Phonebook();
+            return instance;
+        }
+        #endregion
+        #region Конструкторы
+        private Phonebook()
+        {
+
+        }
+        #endregion
     }
 }
